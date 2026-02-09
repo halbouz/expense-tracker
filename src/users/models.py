be0f53101e42bd6656cf201cb986a2981c57b7da
Hany Albouz <hanyalbouz@gmail.com>
2026-02-08 19:50:03 -0500

    Add currency field to Profile model and migrate

--- src/users/models.py
+++ src/users/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.conf import settings
 
-
+CURRENCY_CHOICES = [
+    ("CAD", "Canadian Dollar"),
+    ("USD", "US Dollar"),
+    ("BRL", "Brazilian Real"),
+]
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
-
+    default_currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default="CAD")
+    
     def __str__(self):
         return f'{self.user.username}\'s Profile'
