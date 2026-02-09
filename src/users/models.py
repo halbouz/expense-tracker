bfd9d1004e213c1413dcb9cebd0c99919c89e87f
Hany Albouz <hanyalbouz@gmail.com>
2026-02-08 19:48:08 -0500

    Refactor User model to settings.AUTH_USER_MODEL

--- src/users/models.py
+++ src/users/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
+
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
 
     def __str__(self):
         return f'{self.user.username}\'s Profile'
--- src/users/signals.py
+++ src/users/signals.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -6,7 +6,7 @@ from .models import Profile
 
 # Decorator signifies that when the sender (User) is created and saved,
 # the profile will be created afterwards
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
